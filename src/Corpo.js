84b01e5e95cbcd50d71069a9a5c08dd8125cbd3f
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-08-23 12:59:31 -0300

    componentized repetitive elements

--- src/Corpo.js
+++ src/Corpo.js
@@ -1,236 +1,51 @@
+import Stories from "./Stories";
+import Posts from "./Posts";
+
 export default function Corpo() {
   return (
-    <div class="corpo">
-      <div class="esquerda">
+    <main class="corpo">
+      <Timeline />
+      <Sidebar />
+    </main>
+  );
+}
+
+const Timeline = () => {
+  return (
+    <div class="esquerda">
         <Stories />
         <Posts />
-      </div>
-
-      <div class="sidebar">
-        <Usuario />
-        <Sugestoes />
-
-        <div class="links">
-          Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
-          Hashtags • Idioma
-        </div>
-
-        <div class="copyright">
-          © 2021 INSTAGRAM DO FACEBOOK
-        </div>
-      </div>
     </div>
   );
 }
 
-const Stories = () => {
+const Sidebar = () => {
   return (
-    <div class="stories">
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/9gag.svg" alt="" />
-        </div>
-        <div class="usuario">9gag</div>
+    <div class="sidebar">
+      <Usuario
+        img="../assets/img/catanacomics.svg"
+        arroba="catanacomics"
+        user="Catana"
+      />
+      <Sugestoes />
+
+      <div class="links">
+        Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos •
+        Localizações • Contas mais relevantes • Hashtags • Idioma
       </div>
 
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/meowed.svg" alt="" />
-        </div>
-        <div class="usuario">meowed</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/barked.svg" alt="" />
-        </div>
-        <div class="usuario">barked</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/nathanwpylestrangeplanet.svg" alt="" />
-        </div>
-        <div class="usuario">nathanwpylestrangeplanet</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/wawawicomics.svg" alt="" />
-        </div>
-        <div class="usuario">wawawicomics</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/respondeai.svg" alt="" />
-        </div>
-        <div class="usuario">respondeai</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/filomoderna.svg" alt="" />
-        </div>
-        <div class="usuario">filomoderna</div>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="../assets/img/memeriagourmet.svg" alt="" />
-        </div>
-        <div class="usuario">memeriagourmet</div>
-      </div>
-
-      <div class="setinha">
-        <ion-icon
-          name="chevron-forward-circle"
-          role="img"
-          class="md hydrated"
-          aria-label="chevron forward circle"
-        ></ion-icon>
-      </div>
+      <div class="copyright">© 2021 INSTAGRAM DO FACEBOOK</div>
     </div>
   );
 }
 
-const Posts = () => {
-  return (
-    <div class="posts">
-      <div class="post">
-        <div class="topo">
-          <div class="usuario">
-            <img src="../assets/img/meowed.svg" alt="" />
-            meowed
-          </div>
-          <div class="acoes">
-            <ion-icon
-              name="ellipsis-horizontal"
-              role="img"
-              class="md hydrated"
-              aria-label="ellipsis horizontal"
-            ></ion-icon>
-          </div>
-        </div>
-
-        <div class="conteudo">
-          <img src="../assets/img/gato-telefone.svg" alt="" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon
-                name="heart-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="heart outline"
-              ></ion-icon>
-              <ion-icon
-                name="chatbubble-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="chatbubble outline"
-              ></ion-icon>
-              <ion-icon
-                name="paper-plane-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="paper plane outline"
-              ></ion-icon>
-            </div>
-            <div>
-              <ion-icon
-                name="bookmark-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="bookmark outline"
-              ></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src="../assets/img/respondeai.svg" alt="" />
-            <div class="texto">
-              Curtido por <strong>respondeai</strong> e{" "}
-              <strong>outras 101.523 pessoas</strong>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="post">
-        <div class="topo">
-          <div class="usuario">
-            <img src="../assets/img/barked.svg" alt="" />
-            barked
-          </div>
-          <div class="acoes">
-            <ion-icon
-              name="ellipsis-horizontal"
-              role="img"
-              class="md hydrated"
-              aria-label="ellipsis horizontal"
-            ></ion-icon>
-          </div>
-        </div>
-
-        <div class="conteudo">
-          <img src="../assets/img/dog.svg" alt="" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon
-                name="heart-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="heart outline"
-              ></ion-icon>
-              <ion-icon
-                name="chatbubble-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="chatbubble outline"
-              ></ion-icon>
-              <ion-icon
-                name="paper-plane-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="paper plane outline"
-              ></ion-icon>
-            </div>
-            <div>
-              <ion-icon
-                name="bookmark-outline"
-                role="img"
-                class="md hydrated"
-                aria-label="bookmark outline"
-              ></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src="../assets/img/adorable_animals.svg" alt="" />
-            <div class="texto">
-              Curtido por <strong>adorable_animals</strong> e{" "}
-              <strong>outras 99.159 pessoas</strong>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const Usuario = () => {
+const Usuario = (props) => {
   return (
     <div class="usuario">
-      <img src="../assets/img/catanacomics.svg" alt="" />
+      <img src={props.img} alt="" />
       <div class="texto">
-        <strong>catanacomics</strong>
-        Catana
+        <strong>{props.arroba}</strong>
+        {props.user}
       </div>
     </div>
   );
@@ -244,65 +59,27 @@ const Sugestoes = () => {
         <div>Ver tudo</div>
       </div>
 
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="../assets/img/bad.vibes.memes.svg" alt="" />
-          <div class="texto">
-            <div class="nome">bad.vibes.memes</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="../assets/img/chibirdart.svg" alt="" />
-          <div class="texto">
-            <div class="nome">chibirdart</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="../assets/img/razoesparaacreditar.svg" alt="" />
-          <div class="texto">
-            <div class="nome">razoesparaacreditar</div>
-            <div class="razao">Novo no Instagram</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="../assets/img/adorable_animals.svg" alt="" />
-          <div class="texto">
-            <div class="nome">adorable_animals</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
-
-      <div class="sugestao">
-        <div class="usuario">
-          <img src="../assets/img/smallcutecats.svg" alt="" />
-          <div class="texto">
-            <div class="nome">smallcutecats</div>
-            <div class="razao">Segue você</div>
-          </div>
-        </div>
-
-        <div class="seguir">Seguir</div>
-      </div>
+      <Sugestao img = "../assets/img/bad.vibes.memes.svg" name = "bad.vibes.memes" reason = "Segue você" />
+      <Sugestao img = "../assets/img/chibirdart.svg" name = "chibirdart" reason = "Segue você" />
+      <Sugestao img = "../assets/img/razoesparaacreditar.svg" name = "razoesparaacreditar" reason = "Novo no Instagram" />
+      <Sugestao img = "../assets/img/adorable_animals.svg" name = "adorable_animals" reason = "Segue você" />
+      <Sugestao img = "../assets/img/smallcutecats.svg" name = "smallcutecats" reason = "Segue você" />
     </div>
   );
+}
+
+const Sugestao = (props) => {
+  return (
+    <div class="sugestao">
+        <div class="usuario">
+          <img src={props.img} alt="" />
+          <div class="texto">
+            <div class="nome">{props.name}</div>
+            <div class="razao">{props.reason}</div>
+          </div>
+        </div>
+
+        <div class="seguir">Seguir</div>
+      </div>
+  )
 }
---
+++ src/Stories.js
@@ -0,0 +1,29 @@
+export default function Stories() {
+  return (
+    <div class="stories">
+      <Story img="../assets/img/9gag.svg" user="9gag" />
+      <Story img="../assets/img/meowed.svg" user="meowed" />
+      <Story img="../assets/img/barked.svg" user="barked" />
+      <Story img="../assets/img/nathanwpylestrangeplanet.svg" user="nathanwpylestrangeplanet" />
+      <Story img="../assets/img/wawawicomics.svg" user="wawawicomics" />
+      <Story img="../assets/img/respondeai.svg" user="respondeai" />
+      <Story img="../assets/img/filomoderna.svg" user="filomoderna" />
+      <Story img="../assets/img/memeriagourmet.svg" user="memeriagourmet" />
+
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle" role="img" class="md hydrated" aria-label="chevron forward circle"></ion-icon>
+      </div>
+    </div>
+  );
+}
+
+const Story = (props) => {
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.img} alt="" />
+      </div>
+      <div class="usuario">{props.user}</div>
+    </div>
+  );
+};
